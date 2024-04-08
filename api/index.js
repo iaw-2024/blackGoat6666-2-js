f16bcfe459d60aa0c419149e7b19524ef6d0d195
Iara <iaravera3333@gmail.com>
2024-04-08 13:16:33 -0300

    enfoque express y dom hechos

--- api/index.js
+++ api/index.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const handlebars = require('handlebars');
+const dataPath = './data/data.json';
 
-app.get("/express", (req, res) => res.send("Express on Vercel!"));
+app.get("/express", (req, res) => {
+    indexPath = './public/express/index.html';
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Server Error');
+        } else {
+            const jsonData = JSON.parse(data);
+            fs.readFile(indexPath, 'utf8', (err, html) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('Server Error');
+                } else {
+                    const template = handlebars.compile(html);
+                    const renderedHtml = template(jsonData);
+                    res.send(renderedHtml);
+                }
+            });
+        }
+    });
+});
+
 app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
 app.use(express.static('public'))
 
