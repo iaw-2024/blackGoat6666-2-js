0d8737b0a4bfe5e6771f9b65dde332bcb31c3724
Iara <iaravera3333@gmail.com>
2024-04-09 03:53:29 -0300

    metodo cliente_servidor implementado

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const handlebars = require('handlebars');
+const cors = require('cors');
+const path = require('path');
 const dataPath = './data/data.json';
 
 app.get("/express", (req, res) => {
@@ -26,7 +28,45 @@ app.get("/express", (req, res) => {
     });
 });
 
-app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
+app.get("/cliente_servidor", (req, res) => {
+    indexPath = './public/cliente_servidor/index.html';
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Server Error');
+        } else {
+            fs.readFile(indexPath, 'utf8', (err, html) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('Server Error');
+                } else {
+                    res.send(html);
+                }
+            });
+        }
+    });
+});
+
+app.get("/mounstruos", (req, res) => {
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Server Error');
+        } else {
+            fs.readFile(indexPath, 'utf8', (err, html) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send('Server Error');
+                } else {
+                    const jsonData = JSON.parse(data);
+                    res.json(jsonData);
+                }
+            });
+        }
+    });
+});
+
+app.use(cors());
 app.use(express.static('public'))
 
 
